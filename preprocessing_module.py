223d68e447d184201274c981d3f3590ff817921c
Ryan Park <hp2395@columbia.edu>
2020-11-29 22:03:16 -0500

    updated features used

--- preprocessing_module.py
+++ preprocessing_module.py
@@ -19,39 +19,36 @@ def datacleaning(df):
     df = df[(df.year.isin([2016,2017,2018]))& 
                         df.loan_status.isin(['Fully Paid','Charged Off','Late'])]
     
-    df_new = df[['addr_state','all_util','annual_inc','annual_inc_joint','application_type',
-                             'dti','dti_joint','earliest_cr_line','emp_length','fico_range_high',
-                             'fico_range_low','funded_amnt','funded_amnt_inv','grade','home_ownership',
-                             'initial_list_status','installment','int_rate','issue_d','year','verification_status',
-                             'loan_amnt','loan_status','mort_acc','purpose','open_acc','term']]
+    df_new = df[[
+        'initial_list_status','open_acc','total_acc','inq_last_6mths','collections_12_mths_ex_med','tot_coll_amt',
+        'revol_bal','pub_rec','pub_rec_bankruptcies','acc_now_delinq','all_util','revol_util','delinq_2yrs',
+        'initial_list_status','tot_cur_bal','avg_cur_bal','int_rate','dti','home_ownership','annual_inc','purpose',
+        'earliest_cr_line','emp_length','term','addr_state','installment','mort_acc','application_type',
+        'verification_status','fico_range_low','fico_range_high','issue_d','year','loan_amnt','grade','loan_status']]
+    
     # group some values
     df_new['home_ownership'] = \
-    df_new['home_ownership'].apply(lambda x: np.where((x == 'ANY') | (x == 'NONE'), 'OTHER', x))
+            df_new['home_ownership'].apply(lambda x: np.where((x == 'ANY') | (x == 'NONE'), 'OTHER', x))
     df_new['purpose'] = \
-    df_new['purpose'].apply(lambda x: np.where((x == 'wedding') | (x == 'renewable_energy'), 'other', x))
-
+            df_new['purpose'].apply(lambda x: np.where((x == 'wedding') | (x == 'renewable_energy'), 'other', x))
     df_new['purpose'] = \
-    df_new['purpose'].apply(lambda x: np.where(x == 'house', 'home_improvement', x))
-    # 114 missing all_util
+            df_new['purpose'].apply(lambda x: np.where(x == 'house', 'home_improvement', x))
+    # remove missing all_util, revol_util, inq_last_6mths, avg_cur_bal
     df_new = df_new[df_new['all_util'].notnull()]
-    # take max annual income
-    df_new['annual_inc'] = df_new[['annual_inc','annual_inc_joint']].max(axis=1)
+    df_new = df_new[df_new['revol_util'].notnull()]
+    df_new = df_new[df_new['inq_last_6mths'].notnull()]
+    df_new = df_new[df_new['avg_cur_bal'].notnull()]
     # change value label
     df_new['application_type'] =\
             df_new['application_type'].apply(lambda x: np.where(x == 'Joint App', 'is_joint_app', x))
     # dropping nan and negative values
     df_new['dti'] = df_new['dti'].apply(lambda x: np.where(x<0,np.nan,x))
     df_new = df_new[df_new['dti'].notnull()]
-    # taking the max between dti and dti_joint
-    df_new['dti'] = df_new[['dti','dti_joint']].min(axis=1)
     # number of years since credit line started
     df_new['length_cr_line'] = df_new.year - pd.to_datetime(df_new.earliest_cr_line).dt.year
     # remove rows with missing value 
     df_new = df_new[df_new['emp_length'].notnull()]
-    #creating an average FICO Score 
-    df_new['average_fico'] = df_new[['fico_range_high','fico_range_low']].mean(axis=1)
-    df_new.drop(['year','issue_d','fico_range_high','fico_range_low','earliest_cr_line',
-                        'dti_joint','annual_inc_joint'],1,inplace=True)
+    df_new.drop(['year','issue_d','earliest_cr_line'],1,inplace=True)
     
     return df_new
 
